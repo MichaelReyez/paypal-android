7c2f6d09d5f5a8c085b9095e6b99fef6f5bd41f9
sshropshire <sshropshire@paypal.com>
2022-10-13 10:51:05 -0500

    Try msmaster.

--- Core/src/main/java/com/paypal/android/core/AnalyticsAPI.kt
+++ Core/src/main/java/com/paypal/android/core/AnalyticsAPI.kt
@@ -8,8 +8,10 @@ class AnalyticsAPI {
     private val http = Http()
 
     suspend fun send(event: FPTIEvent) {
+//        val url = URL("https://msmaster.qa.paypal.com:12436/v1/tracking/events")
         val url = URL("https://api.sandbox.paypal.com/v1/tracking/batch/events")
-        val request = HttpRequest(url, HttpMethod.POST, FPTIUploadRequest(listOf(event)).toJSON().toString())
+        val request = HttpRequest(url, HttpMethod.POST, "{}")
+//        val request = HttpRequest(url, HttpMethod.POST, FPTIUploadRequest(listOf(event)).toJSON().toString())
         val response = http.send(request)
         Log.d("Status", response.status.toString())
     }
