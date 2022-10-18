1dd193248559db296c16dc513e3453c7375899df
sshropshire <sshropshire@paypal.com>
2022-10-18 16:52:09 -0500

    Add additonal properties to fpti request.

--- Core/src/main/java/com/paypal/android/core/AnalyticsAPI.kt
+++ Core/src/main/java/com/paypal/android/core/AnalyticsAPI.kt
@@ -8,9 +8,24 @@ class AnalyticsAPI {
     private val http = Http()
 
     suspend fun send(event: FPTIEvent) {
-//        val url = URL("https://msmaster.qa.paypal.com:12436/v1/tracking/events")
-        val url = URL("https://api.sandbox.paypal.com/v1/tracking/batch/events")
-        val request = HttpRequest(url, HttpMethod.POST, "{}")
+        val url = URL("https://api.sandbox.paypal.com/v1/tracking/events/")
+
+        // language=JSON
+        val timestamp = System.currentTimeMillis();
+        val requestBody = """
+            {
+              "events": {
+                "channel": "mobile",
+                "tracking_event": $timestamp,
+                "event_params": {
+                  "e": "la",
+                  "t": $timestamp,
+                  "event_name": "hello_from_team_sdk",
+                }
+              }
+            }
+        """.trimIndent()
+        val request = HttpRequest(url, HttpMethod.POST, requestBody)
 //        val request = HttpRequest(url, HttpMethod.POST, FPTIUploadRequest(listOf(event)).toJSON().toString())
         val response = http.send(request)
         Log.d("Status", response.status.toString())
