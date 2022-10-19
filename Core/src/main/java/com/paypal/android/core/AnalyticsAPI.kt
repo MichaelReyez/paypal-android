866b5920e8d6e2cab7a5f56db82eafcfe1fa43e0
sshropshire <sshropshire@paypal.com>
2022-10-19 15:59:26 -0500

    Add SSL hack workaround for FPTI testing.

--- Core/src/main/java/com/paypal/android/core/AnalyticsAPI.kt
+++ Core/src/main/java/com/paypal/android/core/AnalyticsAPI.kt
@@ -8,19 +8,16 @@ class AnalyticsAPI {
     private val http = Http()
 
     suspend fun send(event: FPTIEvent) {
-        val url = URL("https://api.sandbox.paypal.com/v1/tracking/events/")
-
+        HttpsTrustManager.allowAllSSL();
+        val url = URL("https://msmaster.qa.paypal.com:12436/v1/tracking/events/")
         // language=JSON
         val timestamp = System.currentTimeMillis();
         val requestBody = """
             {
               "events": {
-                "channel": "mobile",
                 "tracking_event": $timestamp,
                 "event_params": {
-                  "e": "la",
-                  "t": $timestamp,
-                  "event_name": "hello_from_team_sdk"
+                  "event_name": "hello_from_team_sdk2"
                 }
               }
             }
